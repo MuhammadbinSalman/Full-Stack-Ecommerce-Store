17fb6afa571f9dd7b51a0693b173abfa5fda4093
Muhammad Bin Salman <muhammad.binyammani@gmail.com>
2023-06-09 14:43:02 +0500

    carousel part
    carousel lorem ipsum dolor mit

--- app/components/Designer.tsx
+++ app/components/Designer.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Designer() {
   return (
-    <div className='px-4 my-6'>
-        <h1 className='font-bold text-left text-4xl leading-3'>
-        Unique and Authentic Vintage Designer Jewellery
+    <div className='w-full px-40 flex justify-end items-start mt-36 h-[500px] bg-[#fbfcff]'>
+        <h1 className='relative top-[-24%] right-6 leading-snug font-bold text-left text-5xl '>
+          Unique and Authentic<br/> Vintage Designer<br/> Jewellery
         </h1>
     </div>
   )
